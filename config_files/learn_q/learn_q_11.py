8018e84f82e13728d2b402d8d06ec97468025893
S. H. Mallick <sam.mallick.97@gmail.com>
2026-01-10 15:43:41 +0100

    config

---
+++ config_files/learn_q/learn_q_11.py
@@ -0,0 +1,43 @@
+import numpy as np
+from mpcrl import ExperienceReplay, UpdateStrategy
+from mpcrl.core.exploration import EpsilonGreedyExploration
+from mpcrl.core.schedulers import ExponentialScheduler
+from mpcrl.optim import NewtonMethod
+
+from config_files.base import BaseConfig
+
+
+class Config(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.id = "learn_q_11"
+
+        self.use_distance_reward = False
+
+        self.layers_path = "mpc/prediction_model/layers_low.mat"
+        self.input_scaler_path = "mpc/prediction_model/input_scaler_low.mat"
+        self.output_scaler_path = "mpc/prediction_model/output_scaler_low.mat"
+
+        # learning
+        self.learn_type = "q_learning"
+        self.learning_rate = 1e-2
+        self.update_strategy = UpdateStrategy(288, hook="on_timestep_end", skip_first=1)
+        self.optimizer = NewtonMethod(learning_rate=self.learning_rate)
+        self.experience = ExperienceReplay(
+            maxlen=288, sample_size=288, include_latest=0
+        )
+        self.exploration = None
+        self.learnable_pars = [
+            "T_ref",
+            "w",
+            "c_t",
+            "V0",
+            "f",
+            "Q",
+            "T_lim_off",
+            "q_lim_off",
+        ]
+
+        np.random.seed(1)
+        load_scale = 0.8 * np.random.random((5, 1)) + 0.6
+        self.P_loads *= load_scale
